9d98a31628d2f0a412bdb0a270e70a942410dad4
taskiranburak1 <burak.taskiran@student.ucll.be>
2024-02-13 12:13:17 +0100

    01 done

--- 01-basic-python/01-arithmetic/03-cakes/student.py
+++ 01-basic-python/01-arithmetic/03-cakes/student.py
@@ -1,1 +1,8 @@
 # Write your code here
+def cakes (eggs, butter, flour):
+  cake1 = eggs // 5
+  cake2 = butter // 250
+  cake3 = flour // 250
+
+  max_amount_cakes = min(cake1, cake2, cake3)
+  return max_amount_cakes
